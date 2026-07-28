8cf7448fd2b36efb17cf5bcb68fd8a5c7f9f502f
ahmedmeo800-dot <ahmedmeo800@gmail.com>
2026-07-28 20:18:59 +0300

    Part 1

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
+import os
+import sqlite3
+import pandas as pd
 
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
+from config import TOKEN, ADMIN_ID
+
+DB_FILE = "students.db"
+
+
+def database_exists():
+    return os.path.exists(DB_FILE)
+
+
+def excel_to_database(file_name):
+    df = pd.read_excel(file_name, dtype=str)
+    df.fillna("", inplace=True)
+
+    conn = sqlite3.connect(DB_FILE)
+
+    df.to_sql("students", conn, if_exists="replace", index=False)
+
+    conn.close()
